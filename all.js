33580f25210a30b0b06f7a8bb886686c0442d4e7
Levia <cr1o0127@gmail.com>
2024-02-16 00:17:42 +0800

    update: change data to API

--- all.js
+++ all.js
@@ -11,43 +11,15 @@ const addTicketNum = document.querySelector("#ticketNum");
 const addTicketRate = document.querySelector("#ticketRate");
 const addTicketDescription = document.querySelector("#ticketDescription");
 
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
+let dataUrl =
+  "https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json";
+
+axios.get(dataUrl).then((res) => {
+  ticketArea.innerHTML = "";
+  res.data.forEach((el) => {
+    update(el);
+  });
+});
 
 function update(obj) {
   ticketArea.innerHTML += `<li class="ticketCard">
@@ -93,8 +65,7 @@ function resetForm() {
 }
 
 btnUpdate.addEventListener("click", () => {
-  let addObj = {
-    id: data.length,
+  let obj = {
     name: addTicketName.value,
     imgUrl: addTicketImg.value,
     area: addTicketRegion.value,
@@ -103,12 +74,16 @@ btnUpdate.addEventListener("click", () => {
     price: addTicketPrice.value,
     rate: addTicketRate.value,
   };
-  ticketArea.innerHTML = "";
-  data.push(addObj);
-  data.forEach((el) => {
-    update(el);
-  });
-  resetForm();
+  if (!addTicketName.value) {
+    alert("請填入套票名稱");
+  } else if (!addTicketRegion.value) {
+    alert("請選擇套票地區");
+  } else {
+    update(obj);
+    const cards = document.querySelectorAll(".ticketCard");
+    searchResult.innerHTML = `本次搜尋共 ${cards.length} 筆資料`;
+    resetForm();
+  }
 });
 
 regionSelector.addEventListener("change", () => {
